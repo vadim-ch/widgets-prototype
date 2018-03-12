c4fc3337469220244827910dfc30a1add7a1fd65
gnome <ivadik05@gmail.com>
2018-03-12 00:29:08 -0700

    fix frame view state middleware

--- src/store/middlewares/frame-state-middleware.js
+++ src/store/middlewares/frame-state-middleware.js
@@ -1,17 +1,7 @@
 import * as actionsCreators from '../actions';
-import { START_DIALOG } from '../actions/start-dialog';
-import { getDialog, getGroups } from '../../io';
-import { getSettings } from '../reducers/settings/selectors';
 import { isDialogAvailable } from '../reducers/dialog/selectors';
-import { isOneGroup } from '../reducers/groups/selectors';
-import { GO_TO_DIALOG } from '../actions/got-to-dialog';
 import { isAvailable } from '../reducers/available/selectors';
-import { SET_DIALOG } from '../actions/set-dialog';
-import { isOpenedFrame } from '../reducers/application/selectors';
-import { SET_AVAILABLE } from '../actions/set-available';
-import { SET_GROUPS } from '../actions/set-groups';
 import { SET_INVITATION } from '../actions/set-invitation';
-import { CLOSE_FRAME } from "../actions/close-frame";
 import { OPEN_FRAME } from "../actions/open-frame";
 
 export const FrameState = {
@@ -22,20 +12,20 @@ export const FrameState = {
 
 export const frameStateMiddleware = store => next => action => {
   switch (action.type) {
-      case SET_INVITATION: {
-          store.dispatch(actionsCreators.setFrameState(FrameState.INVITE));
-          return next(action);
-      }
-      case OPEN_FRAME:
+    case SET_INVITATION: {
+      store.dispatch(actionsCreators.setFrameState(FrameState.INVITE));
+      return next(action);
+    }
+    case OPEN_FRAME:
       // case CLOSE_FRAME:
-        const result = next(action);
-        const state = store.getState();
-          store.dispatch(actionsCreators.setFrameState(
-              (isAvailable(state) || isDialogAvailable(state)) ?
-                  FrameState.ONLINE :
-                  FrameState.OFFLINE
-          ));
-        return result;
+      const result = next(action);
+      const state = store.getState();
+      store.dispatch(actionsCreators.setFrameState(
+          (isAvailable(state) || (isDialogAvailable(state))) ?
+              FrameState.ONLINE :
+              FrameState.OFFLINE
+      ));
+      return result;
     default:
       return next(action);
   }
